0aa249ba242e39e6f57ef34b899825b41dd02748
Anosh D. Ullenius <anosh@anosh.se>
2020-07-22 00:04:34 +0200

    Add JS and HTML checkbox

---
+++ passwd/password.js
@@ -0,0 +1,14 @@
+function plaintext() {
+
+    var passwordBox = document.getElementById("password");
+    console.log(passwordBox);
+    passwordBox.type = "text";
+    console.log(passwordBox);
+}
+
+function init() {
+
+
+
+
+window.onload = plaintext;
