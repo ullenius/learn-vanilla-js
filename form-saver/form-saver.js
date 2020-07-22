78a0f0cec20b5153d913b9a518490f4c20be6c7e
Anosh D. Ullenius <anosh@anosh.se>
2020-07-22 20:09:10 +0200

    Clear localStorage on submit event

--- form-saver/form-saver.js
+++ form-saver/form-saver.js
@@ -4,18 +4,26 @@ var item = Object.create(null);
 
 function init() {
 
-    var cache = localStorage.getItem("fields");
-    if (cache !== null) {
-        var fields = JSON.parse(cache);
-    }
-    console.log(fields);
     var fields = document.getElementsByTagName("input");
+    var saved = readCache();
 
     for (var i = 0; i < fields.length; i++) {
         fields[i].addEventListener("input", save);
+        fields[i].textContent = saved[fields[i].id]; //undefined doesn't break
     }
 
+    var form = document.getElementById("autosave");
+    form.addEventListener("submit", function clearCache() {
+        localStorage.clear();
+    });
+}
 
+function readCache() {
+    var cache = localStorage.getItem("fields");
+    if (cache !== null) {
+        var saved = JSON.parse(cache);
+    }
+    return saved;
 }
 
 function save(event) {
@@ -24,5 +32,6 @@ function save(event) {
     item[event.target.id] = event.target.value;
     localStorage.setItem("fields", JSON.stringify(item));
 }
+
 
 window.onload = init;
