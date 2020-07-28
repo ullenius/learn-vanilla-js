20e4b62d6488324d2c54a2f9a1ae6fab28595dcf
Anosh D. Ullenius <anosh@anosh.se>
2020-07-28 20:21:31 +0200

    Parse genres from HTML

--- checkbox-filters/filters.js
+++ checkbox-filters/filters.js
@@ -4,10 +4,25 @@ function init() {
 
 function readGenres() {
 
-    var playlist = document.getElementById("playlist");
+    var playlist = document.querySelector("#playlist");
+    var childNodes = playlist.childNodes;
 
     console.log(playlist);
+    console.log(childNodes.length);
+
+    var genres = [];
+
+    childNodes.forEach(function(element) {
+
+        if (element.attributes) {
+            let value = element.attributes.getNamedItem("data-genre").value;
+            genres[value] = true;
+        }
+
+    });
+    return genres;
 }
 
+var genres = readGenres();
+console.log(genres);
 
-readGenres();
