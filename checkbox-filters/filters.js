6c2aa87a4c2ef2546160548a9fd06cebaf02caf6
Anosh D. Ullenius <anosh@anosh.se>
2020-07-28 21:25:29 +0200

    Create checkboxes and labels

--- checkbox-filters/filters.js
+++ checkbox-filters/filters.js
@@ -2,7 +2,7 @@ import {SimpleSet} from "./set.js";
 "use strict";
 
 function init() {
-
+    appendCheckboxes();
 }
 
 function readGenres() {
@@ -13,7 +13,7 @@ function readGenres() {
     console.log(playlist);
     console.log(childNodes.length);
 
-    var genres = Object.create(SimpleSet);
+    const genres = Object.create(SimpleSet);
     genres.init();
 
     childNodes.forEach(function(element) {
@@ -22,21 +22,38 @@ function readGenres() {
             let value = element.attributes.getNamedItem("data-genre").value;
             genres.add(value);
         }
-
     });
     return genres.toArray();
 }
 
 function checkboxFactory(name) {
 
-    var box = document.createElement("input");
+    const box = document.createElement("input");
     box.setAttribute("type", "checkbox");
     box.setAttribute("name", name);
+    console.log(box);
     return box;
 }
 
-var genres = readGenres();
-console.log(genres);
+function labelFactory(name) {
+    var label = document.createElement("label");
+    label.setAttribute("for", name);
+    label.textContent = name;
+    return label;
+}
 
+function appendCheckboxes() {
 
+    var filters = document.getElementById("filters");
+    console.log(filters);
+    var genres = readGenres();
+    genres.forEach(function addBox(genre) {
+        let box = checkboxFactory(genre);
+        let label = labelFactory(genre);
+        filters.appendChild(box);
+        filters.appendChild(label);
+    });
 
+}
+
+window.onload = init;
