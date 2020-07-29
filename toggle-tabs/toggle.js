94e06edefe26c3b343815690266ff55a5f218219
Anosh D. Ullenius <anosh@anosh.se>
2020-07-29 22:36:48 +0200

    Hide all other sections using closure

--- toggle-tabs/toggle.js
+++ toggle-tabs/toggle.js
@@ -4,24 +4,36 @@ function init() {
 
     var links = document.getElementsByTagName("a");
     console.log(links);
+    var arr = [];
+
+    function hideAll() { 
+            arr.forEach(function hide(element) {
+                display(element, true);
+        });
+    }
 
     for (let i = 0; i < links.length; i++) {
 
         let target = links[i].href;
+        arr.push(links[i].href);
+
         links[i].addEventListener("click", function show() {
-            display(target);
+            hideAll(); // closure
+            display(target, false);
         });
     }
+    hideAll();
 }
 
-function display(target) {
+function display(target, hidden) {
 
-    var index = target.indexOf("#");
+    const style = (hidden === true) ? "hidden" : "";
 
-    var anchor = target.substring(index+1);
+    var index = target.indexOf("#") + 1;
+    var anchor = target.substring(index);
     console.log(anchor);
     var section = document.getElementById(anchor);
-    section.className = "hidden";
+    section.className = style;
     console.log(section);
 }
 
