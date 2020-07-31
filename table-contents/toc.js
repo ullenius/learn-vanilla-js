122caf69136f839bb9cf41dbf9b3a3b7cadade08
Anosh D. Ullenius <anosh@anosh.se>
2020-07-31 18:55:33 +0200

    Creating simple TOC list

--- table-contents/toc.js
+++ table-contents/toc.js
@@ -2,16 +2,30 @@
 
 function init() {
 
+    var toc = document.getElementById("toc");
     var headings = document.getElementsByTagName("h2");
     var arr = [];
 
-    for (let i = 0; i < headings; i++) {
+    console.log(headings);
+
+    for (let i = 0; i < headings.length; i++) {
 
         let text = headings[i].textContent;
         arr.push(text);
     }
 
-    console.log(arr);
+    var ol = document.createElement("ol");
+    toc.appendChild(ol);
+
+    arr.forEach(function makeToc(element) {
+
+        let li = document.createElement("li");
+        let text = document.createTextNode(element);
+        li.appendChild(text);
+        ol.appendChild(li);
+    });
+
+
 
 }
 
