03149bd6b7b94146033adea47922626bda4a1bdf
Anosh D. Ullenius <anosh@anosh.se>
2020-08-01 14:11:35 +0200

    Add external JS

---
+++ monster-game/monster.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function init() {
+
+	var monsters = [
+		'sock!',
+		'monster1.svg',
+		'monster2.svg',
+		'monster3.svg',
+		'monster4.svg',
+		'monster5.svg',
+		'monster6.svg',
+		'monster7.svg',
+		'monster8.svg',
+		'monster9.svg',
+		'monster10.svg',
+		'monster11.svg'
+	];
+
+}
