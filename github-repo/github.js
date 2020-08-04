48aad58fbf4eb23afb54460c726a08d930a79b17
Anosh D. Ullenius <anosh@anosh.se>
2020-08-04 21:17:25 +0200

    Print repo contents

--- github-repo/github.js
+++ github-repo/github.js
@@ -4,6 +4,52 @@ import {repos} from "./repos.js";
 
 function init() {
 
+    console.log(repos.length);
+    console.log(repos[0]);
+
+    var app = document.getElementById("app");
+    
+    repos.forEach(function printRepo(repo) {
+
+
+        if (repo.fork === false) {
+
+            const name = header(repo.name);
+            const language = paragraph("Language: " + repo.language);
+            const description = paragraph("Description: " + repo.description);
+
+            const arr = [name, language, description];
+            const article = createArticle(arr);
+            app.appendChild(article);
+        }
+    });
+}
+
+function paragraph(text) {
+    return createElement("p", text);
+}
+
+function header(text) {
+    return createElement("h1", text);
+}
+
+function createElement(type, text) {
+    const element = document.createElement(type);
+    const content = document.createTextNode(text);
+    element.appendChild(content);
+
+    Object.freeze(element);
+    return element;
+}
+
+function createArticle(contents) {
+
+    const article = document.createElement("article");
+    contents.forEach(function append(element) {
+        article.appendChild(element);
+        });
+    Object.freeze(article);
+    return article;
 }
 
 window.onload = init;
