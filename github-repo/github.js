1afecd18103aad6fe6fd76e347e112b6a13f1376
Anosh D. Ullenius <anosh@anosh.se>
2020-08-06 18:55:55 +0200

    Caching results in localStorage

--- github-repo/github.js
+++ github-repo/github.js
@@ -2,17 +2,41 @@
 
 const RECENT_COMMITS = 3;
 const REPO_URL = "https://api.github.com/users/ullenius/repos";
+const SHA_LENGTH = 7;
 const HTTP_OK = 200;
 
 function init() {
 
+    var cache = readCache("repos");
+    if (cache) {
+        console.log("displaying repos from cache");
+        displayRepos(cache);
+    } else {
+        console.log("fetching repos from API");
+        fetchRepos();
+    }
+}
+
+function readCache(name) {
+
+    var cache = localStorage.getItem(name);
+    if (cache !== null)
+        var saved = JSON.parse(cache);
+    return saved;
+}
+
+function saveCache(name, data) {
+    var json = JSON.stringify(data);
+    localStorage.setItem(name, json);
+}
+
+function fetchRepos() {
     var xhr = ApiRequest(REPO_URL);
     xhr.send();
     xhr.onload = function callback() {
-
         if (xhr.status === HTTP_OK) {
-
             var repos = JSON.parse(xhr.responseText);
+            saveCache("repos", repos); 
             displayRepos(repos);
         } else {
             console.log("Something went wrong fetching the repos :(");
@@ -48,25 +72,35 @@ function displayRepos(repos) {
 
 function displayCommits(name, url) {
 
-    const SHA_LENGTH = 7;
-    var xhr = ApiRequest(url);
-    xhr.send();
+    var cache = readCache(name);
+    if (cache) {
+        console.log("reading commits from cache");
+        display(cache);
+    } else {
+        console.log("fetching commits");
+        var xhr = ApiRequest(url);
+        xhr.send();
 
-    xhr.onload = function callback() {
-        if (xhr.status === HTTP_OK) {
-            var result = JSON.parse(xhr.responseText);
-            var article = document.getElementById(name);
-            for (let i = 0; i < RECENT_COMMITS; i++) {
-                let sha = result[i].sha.substring(0,SHA_LENGTH);
-                let message = result[i].commit.message;
-                let text = paragraph(sha + " : " + message);
-                article.appendChild(text);
+        xhr.onload = function callback() {
+            if (xhr.status === HTTP_OK) {
+                var result = JSON.parse(xhr.responseText);
+                saveCache(name, result);
+                display(result);
+            } else {
+                console.log("Something went wrong");
+                console.log("HTTP status: " + xhr.status);
             }
-        } else {
-            console.log("Something went wrong");
-            console.log("HTTP status: " + xhr.status);
+        };
+    }
+    function display(result) {
+        var article = document.getElementById(name);
+        for (let i = 0; i < RECENT_COMMITS; i++) {
+            let sha = result[i].sha.substring(0,SHA_LENGTH);
+            let message = result[i].commit.message;
+            let text = paragraph(sha + " : " + message);
+            article.appendChild(text);
         }
-    };
+    }
 }
 
 function ApiRequest(url) {
